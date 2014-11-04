e0214fb3f60774978b2773113444e734db319179
Amanda Dillon <amanda.c.dillon@gmail.com>
2014-11-04 15:45:55 -0800

    modify single post template and related content

--- single.php
+++ single.php
@@ -2,12 +2,14 @@
 /**
  * Single post template
  *
+ * This template controls what appears for a single post view!
+ *
  * @package Gracie
  */
 
 get_header(); ?>
 
-<section id="primary">
+<section id="primary" role="main">
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,5 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-   
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
     <header class="entry-header">
         <h1 class="entry-title">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
@@ -9,12 +8,13 @@
 
     <div class="entry-content">
         <?php the_content(); ?>
-        <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'gracie' ) . '</span>', 'after' => '</div>' ) ); ?>
     </div><!-- .entry-content -->
 
     <footer class="entry-meta">
-
-        <?php the_tags( '<div class="post-tags">' . __( 'Tagged: ', 'gracie' ) , ', ', '</div>' ); ?>
+        <div class="post-category">
+            <?php the_category( ' / '); ?>
+        </div>
+    
 
         <div class="comments-link">
             <?php comments_popup_link( 
